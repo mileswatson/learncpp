f08b242440a6a0d50c5c4da1a3d9ebb2efe60c76
Miles Watson <contact@mileswatson.net>
2023-03-26 20:01:52 +0100

    Added tests.

---
+++ tests/square_test.cpp
@@ -0,0 +1,10 @@
+#include <gtest/gtest.h>
+#include <learncpp/square/square.hpp>
+
+// Demonstrate some basic assertions.
+TEST(SquareTest, BasicAssertions)
+{
+  EXPECT_EQ(square(0), 0);
+  EXPECT_EQ(square(5), 25);
+  EXPECT_EQ(square(-2), 4);
+}
