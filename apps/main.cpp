6b79fbe155b791c8fefafb68d0119ff72c249690
Miles Watson <contact@mileswatson.net>
2023-03-26 19:24:53 +0100

    Created project structure.

---
+++ apps/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include <learncpp/square/square.hpp>
+
+using namespace std;
+
+int main()
+{
+    cout << square(5) << endl;
+    return 0;
+}
---
+++ src/learncpp/square/square.cpp
@@ -0,0 +1,6 @@
+#include "square.hpp"
+
+int square(int n)
+{
+    return n * n;
+}
