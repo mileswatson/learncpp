604ee09a82d57ce0fb5b7d127075dade7a08d2cd
Miles Watson <contact@mileswatson.net>
2023-03-27 19:41:45 +0100

    Added custom ID with integer specialisation.

--- src/learncpp/automata/nfa.hpp
+++ src/learncpp/automata/nfa.hpp
@@ -12,54 +12,99 @@ using namespace std;
 
 namespace automata
 {
-    template <typename T>
-    class NfaNode
-    {
-    private:
-        static inline int nextId = 0;
-        int id = nextId++;
-        unordered_map<optional<T>, unordered_set<NfaNode<T> *>> connections;
-        NfaNode(const NfaNode<T> &) = delete;
-        operator=(const NfaNode<T> &) = delete;
-        NfaNode(NfaNode<T> &&) = delete;
-        operator=(NfaNode<T> &&) = delete;
+    template <typename T, typename I = int, bool fully_defined = true>
+    class NfaNode;
 
-    private:
-        void visit_epsilon_closure(unordered_set<NfaNode<T> *> &visited)
+    template <typename T, typename I>
+    class NfaNode<T, I, false>
+    {
+    protected:
+        I id;
+        unordered_map<optional<T>, unordered_set<NfaNode<T, I> *>> connections;
+
+        NfaNode(const NfaNode<T, I> &) = delete;
+        operator=(const NfaNode<T, I> &) = delete;
+        NfaNode(NfaNode<T, I> &&) = delete;
+        operator=(NfaNode<T, I> &&) = delete;
+
+    public:
+        NfaNode(T id) : id(id)
+        {
+        }
+
+        const I &get_id()
+        {
+            return id;
+        }
+        void add_connection(optional<T> input, NfaNode<T, I> *destination)
+        {
+            unordered_set<NfaNode<T, I> *> &s = connections[input];
+            s.emplace(destination);
+        }
+
+        const unordered_set<NfaNode<T, I> *> &next(optional<T> input)
+        {
+            return connections[input];
+        }
+
+        virtual void visit_epsilon_closure(unordered_set<NfaNode<T, I> *> &visited) = 0;
+
+        unordered_set<NfaNode<T, I> *> epsilon_closure()
+        {
+            unordered_set<NfaNode<T, I> *> visited;
+            visit_epsilon_closure(visited);
+            return visited;
+        }
+    };
+
+    template <typename T, typename I>
+    class NfaNode<T, I, true> : public NfaNode<T, I, false>
+    {
+    public:
+        void visit_epsilon_closure(unordered_set<NfaNode<T, I> *> &visited) override
         {
             if (!visited.emplace(this).second)
                 return;
-            const unordered_set<NfaNode<T> *> &toVisit = connections[{}];
+            const unordered_set<NfaNode<T, I> *> &toVisit = this->connections[{}];
             for (auto &v : toVisit)
             {
                 v->visit_epsilon_closure(visited);
             }
         }
+    };
+
+    template <typename T>
+    class NfaNode<T, int, true> : public NfaNode<T, int, false>
+    {
+        static inline int nextId = 0;
 
     public:
-        NfaNode() {}
-        int get_id()
-        {
-            return id;
-        }
-        void add_connection(optional<T> input, NfaNode<T> *destination)
-        {
-            unordered_set<NfaNode<T> *> &s = connections[input];
-            s.emplace(destination);
-        }
+        NfaNode() : NfaNode<T, int, false>(nextId++) {}
 
-        const unordered_set<NfaNode<T> *> &next(optional<T> input)
+        void visit_epsilon_closure(unordered_set<NfaNode<T> *> &visited) override
         {
-            return connections[input];
-        }
-
-        unordered_set<NfaNode<T> *> epsilon_closure()
-        {
-            unordered_set<NfaNode<T> *> visited;
-            visit_epsilon_closure(visited);
-            return visited;
+            if (!visited.emplace(this).second)
+                return;
+            const unordered_set<NfaNode<T> *> &toVisit = this->connections[{}];
+            for (auto &v : toVisit)
+            {
+                v->visit_epsilon_closure(visited);
+            }
         }
     };
+
+    /*
+        template <typename T, typename I>
+        void NfaNode<T, I>::visit_epsilon_closure(unordered_set<NfaNode<T, I> *> &visited)
+        {
+            if (!visited.emplace(this).second)
+                return;
+            const unordered_set<NfaNode<T, I> *> &toVisit = this->connections[{}];
+            for (auto &v : toVisit)
+            {
+                v->visit_epsilon_closure(visited);
+            }
+        }*/
 
     template <typename T>
     class Nfa;
